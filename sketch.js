5b9e7720f670165fdd7434746e94bd357aef7df7
Aakash6188 <90509234+Aakash6188@users.noreply.github.com>
2021-09-18 17:35:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+var a = prompt("Enter the first number")
+var b = prompt("Enter the second number")
+let temp;
+temp=a;
+a=b;
+b= temp
+console.log("The number after swapping is" + a);
+console.log("The number after swapping is" + b);
+console.log("hello");
